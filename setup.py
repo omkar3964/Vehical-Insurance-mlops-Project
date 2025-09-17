b9bb248e9f6cd60fdcc15294a9bc84f713f967ff
omkar3964 <omalbhare@gmail.com>
2025-09-17 11:36:35 +0530

    1 project structure ready. creating venv with install dependencies.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="omkar",
+    author_email="omkarmalbhare29@gmail.com",
+    packages=find_packages()
+)
